95aca467314d01519a77c9a864bfdd7b1eb7c442
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2022-06-10 11:56:39 -0300

    init: inicio do projeto

---
+++ modulo1/projeto-labefy/projeto-labefy/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components"
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+function App() {
+  return (
+    <Container>
+      <h1>Coming soon</h1>
+    </Container>
+  );
+}
+
+export default App;
