73da83be8d8bf0b1bb8deb3aeb7dc53b797af8d9
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2022-08-04 11:28:38 -0300

    init: primeiro commit

---
+++ modulo3/intro-typescript/src/exercicio6.ts
@@ -0,0 +1,22 @@
+// Faça uma função que receba dois números como parâmetros e imprima
+// no terminal, as seguintes informações:
+
+// a) A soma desses números
+
+// b) A subtração desses números
+
+// c) A multiplicação desses números
+
+// d) Qual deles é o maior
+
+// Você pode fazer todas as operações na mesma função.
+
+// Como estamos usando o TypeScript, devemos passar para o código o tipo
+// de parâmetro que é esperado pela função, afinal, no javascript, nada
+// nos impede de passar parâmetros sem especificação. É como diz aquela
+// piada de programador:
+// "Alguém pode me explicar TypeScript como se eu fosse uma criança de
+// 5 anos?"
+// "Se você disser 'laranja', o TypeScript vai te perguntar se você está
+// falando da fruta ou da cor. O JavaScript não vai te perguntar nada e
+// você pode acabar bebendo um copo de tinta laranja."
---
+++ modulo3/intro-typescript/src/exercicio8.ts
@@ -0,0 +1,3 @@
+// a) Escreva uma função que receba uma string e retorne a string
+// reversa. Em outras palavras, se o input da sua função for "abcd",
+// a saída deve ser "dcba".
