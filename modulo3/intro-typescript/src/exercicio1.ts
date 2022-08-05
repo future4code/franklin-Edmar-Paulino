5be48cf9bf6a346151864ff28d51664aeeeeb273
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2022-08-05 10:27:47 -0300

    feat: exercicios sem desafios finalizados

--- modulo3/intro-typescript/src/exercicio1.ts
+++ modulo3/intro-typescript/src/exercicio1.ts
@@ -12,3 +12,13 @@
 //       return "Isósceles";
 //     }
 //   }
+
+function checaTriangulo(a: number, b: number, c: number): string {
+    if (a !== b && b !== c) {
+        return "Escaleno";
+    } else if (a === b && b === c) {
+        return "Equilátero";
+    } else {
+        return "Isósceles";
+    }
+}
--- modulo3/intro-typescript/src/exercicio2.ts
+++ modulo3/intro-typescript/src/exercicio2.ts
@@ -8,3 +8,7 @@
 //     const cor3 = prompt("Insira sua terceira cor favorita")
 //     console.log([cor1, cor2, cor3])
 //  }
+
+function imprimeTresCoresFavoritas(cor1: string, cor2: string, cor3: string): void {
+    console.log([cor1, cor2, cor3]);
+}
