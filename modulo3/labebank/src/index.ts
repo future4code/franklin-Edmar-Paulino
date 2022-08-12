97d7db10bbd2695f93e17a20660b61a3261c0c86
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2022-08-12 14:20:36 -0300

    feat: esboço inicial

--- modulo3/labebank/src/index.ts
+++ modulo3/labebank/src/index.ts
@@ -3,13 +3,37 @@ import cors from "cors";
 
 const app = express();
 
+// Server config
 app.use(cors());
 app.use(express.json());
 
 
+// tipos
+type Transaction = {
+    
+}
+
+type Account = {
+
+}
+
+
+// endpoints
 app.get("/", (request: Request, response: Response) => {
     response.status(200).send({ message: "Hello world" });
 });
 
+// criar conta
 
+// pegar saldo
+
+// adicionar saldo
+
+// pagar conta
+
+// transferência interna
+
+
+
+// Server init
 app.listen("3003", () => console.log("API is running..."));
