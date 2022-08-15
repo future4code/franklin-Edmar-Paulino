7339cb53681fa6b61cc665ac55e8ac8b3db926f3
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2022-08-15 11:57:29 -0300

    feat: funcionalidade de fazer depósito

--- modulo3/labebank/src/index.ts
+++ modulo3/labebank/src/index.ts
@@ -93,7 +93,7 @@ app.post("/users", (req: Request, res: Response) => {
     const { name, cpf, birth } = req.body;
     if (name && cpf && birth) {
         if (isOfAge(birth)) {
-            if (!users.find((account: Account) => account.cpf === cpf)) {
+            if (!users.find((account: Account): any => account.cpf === cpf)) {
                 const newAccount: Account = createNewAccount(name, cpf, birth);
                 users.push(newAccount);
                 res.status(201).send({ message: "Cliente cadastrado com sucesso." });
@@ -112,7 +112,7 @@ app.post("/users", (req: Request, res: Response) => {
 app.get("/balance", (req: Request, res: Response) => {
     const { name, cpf } = req.body;
     if (name && cpf) {
-        const account = users.find((account: Account) => account.name === name && account.cpf === cpf);
+        const account = users.find((account: Account): any => account.name === name && account.cpf === cpf);
         if (account) {
             res.status(200).send({ balance: account.balance });
         } else {
@@ -124,9 +124,21 @@ app.get("/balance", (req: Request, res: Response) => {
 });
 
 // adicionar saldo
-// app.put("/balance", (req: Request, res: Response) => {
-
-// });
+app.put("/balance", (req: Request, res: Response) => {
+    const { name, cpf, value } = req.body;
+    if (name && cpf && value > 0) {
+        const account = users.find((account: Account): any => account.name === name && account.cpf === cpf);
+        if (account) {
+            account.balance += value;
+            createTransaction(account, value, "Depósito bancário", "");
+            res.status(201).send({ message: `Depósito efetuado. Novo saldo: ${account.balance}` });
+        } else {
+            res.status(404).send({ message: "Cliente não econtrado no sistema" });
+        }
+    } else {
+        res.status(400).send({ message: "É necessário informar o nome, cpf e valor maior que R$ 0,00 para efetuar o depósito" });
+    }
+});
 
 // pagar conta
 // app.put("/pay", (req: Request, res: Response) => {
@@ -144,19 +156,43 @@ app.listen("3003", () => console.log("API is running..."));
 
 // funções
 function isOfAge(birth: string): boolean {
-    const [ day, month, year ] = birth.split("/");
+    const [ birthDay, birthMonth, birthYear ] = birth.split("/").map((item: string): number => Number(item));
+
     const date: Date = new Date();
-    if (date.getFullYear() - Number(year) > 18)
-        return true;
-    if (date.getFullYear() - Number(year) === 18 && (date.getMonth() + 1) > Number(month))
-        return true;
-    if (date.getFullYear() - Number(year) === 18 && (date.getMonth() + 1) === Number(month) && date.getDate() >= Number(day))
-        return true;
-    return false;
+    const currentYear = date.getFullYear();
+    const currentMonth = date.getMonth() + 1;
+    const currentDay = date.getDate();
+
+    return (currentYear - birthYear > 18)
+        || (currentYear - birthYear === 18 && currentMonth >birthMonth)
+        || (currentYear - birthYear === 18 && currentMonth ===birthMonth && currentDay >= birthDay);
+}
+
+function isPresentOrFuture(date: string): boolean {
+    const [ day, month, year ] = date.split("/").map((item: string): number => Number(item));
+
+    const currentDate: Date = new Date();
+    const currentYear: number = currentDate.getFullYear();
+    const currentMonth: number = currentDate.getMonth() + 1;
+    const currentDay: number = currentDate.getDate();
+
+    return (currentYear < year)
+        || (currentYear === year && currentMonth < month)
+        || (currentYear === year && currentMonth === month && currentDay <= day);
 }
 
 function createNewAccount(name: string, cpf: string, birth: string): Account {
     const transactions: Transaction[] = [];
     const newAccount: Account = { name, cpf, birth, balance: 0, transactions};
     return newAccount;
+}
+
+function createTransaction(account: Account, value: number, description: string, date: string): void {
+    const dateObj = new Date();
+    const newTransaction: Transaction = {
+        value,
+        date: date ? date : `${dateObj.getDate()}/${(dateObj.getMonth() + 1)}/${dateObj.getFullYear()}`,
+        description
+    };
+    account.transactions.push(newTransaction);
 }
