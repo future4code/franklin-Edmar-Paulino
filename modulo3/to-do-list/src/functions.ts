932fd4a47502ae6b537efb858d9253208f0ba761
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2022-08-26 14:11:29 -0300

    finalizado criar task e pegar task pelo id - formatação da data não está correta ainda

--- modulo3/to-do-list/src/functions.ts
+++ modulo3/to-do-list/src/functions.ts
@@ -1,8 +1,8 @@
 const ALFANUM: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-function createHash(): string {
-    let hash: string = "";
-    for (let i: number = 0; i < 32; i++) {
+function createHash():string {
+    let hash:string = "";
+    for (let i:number = 0; i < 32; i++) {
         hash = hash.concat(ALFANUM.charAt((Date.now() * Math.random()) % 62));
         if (i >= 8 && i <= 20 && i % 4 === 0) {
             hash = hash.concat("-");
--- modulo3/to-do-list/src/index.ts
+++ modulo3/to-do-list/src/index.ts
@@ -3,57 +3,130 @@ import { Request, Response } from "express";
 import connection from "./connection";
 import { createHash } from "./functions";
 
-app.post("/user", async (req: Request, res: Response) => {
-    let errorStatus = 500;
+let errorStatus:number = 500;
+
+async function createUser(name:string, nickname:string, email:string):Promise<void> {
+    if (!name || !nickname || !email) {
+        errorStatus = 400;
+        throw new Error("Favor informar nome, apelido e email.");
+    }
+    const id:string = createHash();
+    await connection("TodoListUser")
+    .insert({ id, name, nickname, email})
+    .into("TodoListUser");
+}
+
+async function getUserById(id:string):Promise<any> {
+    const [result] = await connection("TodoListUser")
+    .select(["id", "nickname"])
+    .from("TodoListUser")
+    .where({ id });
+    if (!result) {
+        errorStatus = 404;
+        throw new Error("Usuário não encontrado.");
+    }
+    return result;
+}
+
+async function editUser(id:string, newName:string, newNickname:string):Promise<void> {
+    if (!newName || !newNickname) {
+        errorStatus = 400;
+        throw new Error("Favor informar nome e apelido.");
+    }
+    await connection("TodoListUser")
+    .update({ name: newName, nickname: newNickname })
+    .where({ id });
+}
+
+async function createTask(title:string, description:string, limitDate:string, creatorUserId:string):Promise<void> {
+    if (!title || !description || !limitDate || !creatorUserId) {
+        errorStatus = 404;
+        throw new Error("Favor informar titulo, descrição, data limite e ID do usuário criador da tarefa")
+    }
+    const [day, month, year] = limitDate.split("/");
+    const date:Date = new Date(Number(year), Number(month), Number(day));
+    const id:string = createHash();
+    await connection("TodoListTask")
+    .insert({ id, title, description, limit_date: date, creator_user_id: creatorUserId})
+    .into("TodoListTask");
+}
+
+async function getTaskById(id:string):Promise<any> {
+    const [result] = await connection("TodoListTask")
+    .select([
+        "TodoListTask.id AS taskId",
+        "TodoListTask.title",
+        "TodoListTask.description",
+        "TodoListTask.limit_date AS limitDate",
+        "TodoListTask.status",
+        "TodoListTask.creator_user_id AS creatorUserId",
+        "TodoListUser.nickname AS creatorUserNickname"
+    ])
+    .from("TodoListTask")
+    .where({ "TodoListTask.id": id })
+    .join("TodoListUser", {"TodoListTask.creator_user_id": "TodoListUser.id"})
+    if (!result) {
+        errorStatus = 404;
+        throw new Error("Usuário não econtrado.")
+    }
+    // Não está funcionando como deveria
+    const limitDate:string = `${result.limitDate.getDate()}/${result.limitDate.getMonth()}/${result.limitDate.getFullYear()}`;
+    result.limitDate = limitDate;
+    return result;
+}
+
+app.post("/user", async (req:Request, res:Response):Promise<void> => {
     try {
         const { name, nickname, email} = req.body;
-        if (!name || !nickname || !email) {
-            errorStatus = 400;
-            throw new Error("Favor informar nome, apelido e email.");
-        }
-        await connection("TodoListUser")
-        .insert({ id: createHash(), name, nickname, email})
-        .into("TodoListUser");
-        res.status(200).end();
-    } catch(error: any) {
+        await createUser(name, nickname, email);
+        res.status(201).send({ message: "Usuário criado com sucesso" });
+    } catch(error:any) {
         console.log(error);
         res.status(errorStatus).send(error.message);
     }
 });
 
-app.get("/user/:id", async (req: Request, res: Response) => {
-    let errorStatus = 500;
+app.get("/user/:id", async (req:Request, res:Response):Promise<void> => {
     try {
-        const [result] = await connection("TodoListUser")
-        .select(["id", "nickname"])
-        .from("TodoListUser")
-        .where({ id: req.params.id  });
-        console.log(typeof result)
-        if (!result) {
-            errorStatus = 404;
-            throw new Error("Usuário não encontrado.");
-        }
+        const { id } = req.params;
+        const result:any = await getUserById(id);
         res.status(200).send(result);
-    } catch(error: any) {
+    } catch(error:any) {
         console.log(error);
         res.status(errorStatus).send(error.message);
     }
 });
 
-app.put("/user/edit/:id", async (req: Request, res: Response) => {
-    let errorStatus = 500;
+app.put("/user/edit/:id", async (req:Request, res:Response):Promise<void> => {
     try {
+        const { id } = req.params;
         const { name, nickname } = req.body;
-        if (!name || !nickname) {
-            errorStatus = 400;
-            throw new Error("Favor informar nome e apelido.");
-        }
-        await connection("TodoListUser")
-        .update({ name, nickname })
-        .where({ id: req.params.id });
-        res.status(201).end();
-    } catch(error: any) {
+        await editUser(id, name, nickname);
+        res.status(201).send({ message: "Alterações salvas com sucesso" });
+    } catch(error:any) {
         console.log(error);
+        res.status(errorStatus).send(error.message);
+    }
+});
+
+app.post("/task", async (req:Request, res:Response):Promise<void> => {
+    try {
+        const { title, description, limitDate, creatorUserId } = req.body;
+        await createTask(title, description, limitDate, creatorUserId);
+        res.status(201).send({ message: "Tarefa criada com sucesso" });
+    } catch(error:any) {
+        console.log(error.message);
+        res.status(errorStatus).send(error.message);
+    }
+});
+
+app.get("/task/:id", async (req:Request, res:Response):Promise<void> => {
+    try {
+        const { id } = req.params;
+        const result:any = await getTaskById(id);
+        res.status(200).send(result);
+    } catch(error:any) {
+        console.log(error.message);
         res.status(errorStatus).send(error.message);
     }
 });
