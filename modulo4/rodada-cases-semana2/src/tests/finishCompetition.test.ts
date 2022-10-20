6e792bf4c0ac3f697dcae5c1fcaa4a80fa18d731
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2022-10-20 10:24:18 -0300

    finish finishCompetition function tests

--- modulo4/rodada-cases-semana2/src/tests/finishCompetition.test.ts
+++ modulo4/rodada-cases-semana2/src/tests/finishCompetition.test.ts
@@ -1,5 +1,78 @@
+import CompetitionBusiness from "../business/CompetitionBusiness";
+import { IIDInputDTO, IMessageOutputDTO } from "../model/Competition";
+import CompetitionDatabaseMock from "./mocks/CompetitionDatabaseMock";
+import IdGeneratorMock from "./mocks/IdGeneratorMock";
+
 describe("finishCompetition", () => {
-    test("true should be true", async () => {
-        expect(true).toBe(true);
+    const competitionBusiness: CompetitionBusiness = new CompetitionBusiness(
+        new CompetitionDatabaseMock(),
+        new IdGeneratorMock()
+    );
+
+    test("Should throw the error ID da competição não informado for an empty ID", async () => {
+        expect.assertions(1);
+        try {
+            const input: IIDInputDTO = { id: "" };
+            await competitionBusiness.finishCompetition(input);
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                expect(error.message).toBe("ID da competição não informado");
+            }
+        }
+    })
+
+    test("Should throw the error Competição não encontrada for an invalid ID", async () => {
+        expect.assertions(1);
+        try {
+            const competitionBusiness: CompetitionBusiness = new CompetitionBusiness(
+                new CompetitionDatabaseMock(true),
+                new IdGeneratorMock()
+            );
+            const input: IIDInputDTO = { id: "d98ecd2d-a77d-4004-8ec7-371a6d839229" };
+            await competitionBusiness.finishCompetition(input);
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                expect(error.message).toBe("Competição não encontrada");
+            }
+        }
+    })
+
+    test("Should throw the error Competição já finalizada for an finished competition", async () => {
+        expect.assertions(1);
+        try {
+            const competitionBusiness: CompetitionBusiness = new CompetitionBusiness(
+                new CompetitionDatabaseMock(false, false, false, true),
+                new IdGeneratorMock()
+            );
+            const input: IIDInputDTO = { id: "d98ecd2d-a77d-4004-8ec7-371a6d839229" };
+            await competitionBusiness.finishCompetition(input);
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                expect(error.message).toBe("Competição já finalizada");
+            }
+        }
+    })
+
+    test("Should throw the error Competição não finalizada for none affected rows in database", async () => {
+        expect.assertions(1);
+        try {
+            const competitionBusiness: CompetitionBusiness = new CompetitionBusiness(
+                new CompetitionDatabaseMock(false, false, false, false, 1, 0),
+                new IdGeneratorMock()
+            );
+            const input: IIDInputDTO = { id: "d98ecd2d-a77d-4004-8ec7-371a6d839229" };
+            await competitionBusiness.finishCompetition(input);
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                expect(error.message).toBe("Competição não finalizada");
+            }
+        }
+    })
+    
+    test("Should return a successful message for correct input", async () => {
+        const input: IIDInputDTO = { id: "d98ecd2d-a77d-4004-8ec7-371a6d839229" };
+        const output: IMessageOutputDTO = await competitionBusiness.finishCompetition(input);
+        const expectedOutput: IMessageOutputDTO = { message: "Competição finalizada com sucesso" };
+        expect(output).toMatchObject(expectedOutput);
     })
 });
